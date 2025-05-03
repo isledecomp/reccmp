c8a16410f138c80872122c3c911b9a1cf317c1ff
Piotr Halama <skrzynka@halamix2.pl>
2025-05-03 20:29:52 +0200

    change generation settings to use the base names

--- generate_schemas.py
+++ generate_schemas.py
@@ -5,9 +5,9 @@ projectFile = config.ProjectFile(targets={})
 userFile = config.UserFile(targets={})
 buildFile = config.BuildFile(project="", targets={})
 
-projectSchema = projectFile.model_json_schema()
-userSchema = userFile.model_json_schema()
-buildSchema = buildFile.model_json_schema()
+projectSchema = projectFile.model_json_schema(by_alias=False)
+userSchema = userFile.model_json_schema(by_alias=False)
+buildSchema = buildFile.model_json_schema(by_alias=False)
 
 with open("project-schema.json", "wt", encoding="utf-8") as schema:
     schema.write(json.dumps(projectSchema, indent=2))
