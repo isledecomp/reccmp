ac2bb4e1889eb401c749cc458acbbe090fa5e100
Piotr Halama <skrzynka@halamix2.pl>
2025-05-03 20:23:00 +0200

    generate schemas autoamtically

---
+++ generate_schemas.py
@@ -0,0 +1,19 @@
+import json
+import reccmp.project.config as config
+
+projectFile = config.ProjectFile(targets={})
+userFile = config.UserFile(targets={})
+buildFile = config.BuildFile(project="", targets={})
+
+projectSchema = projectFile.model_json_schema()
+userSchema = userFile.model_json_schema()
+buildSchema = buildFile.model_json_schema()
+
+with open("project-schema.json", "wt", encoding="utf-8") as schema:
+    schema.write(json.dumps(projectSchema, indent=2))
+
+with open("user-schema.json", "wt", encoding="utf-8") as schema:
+    schema.write(json.dumps(userSchema, indent=2))
+
+with open("build-schema.json", "wt", encoding="utf-8") as schema:
+    schema.write(json.dumps(buildSchema, indent=2))
