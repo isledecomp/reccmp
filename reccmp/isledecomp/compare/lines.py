918db231aa9173d6832743b98f71b4377962b89c
disinvite <disinvite@users.noreply.github.com>
2025-05-25 14:43:41 -0400

    Keep using dict for now

--- reccmp/isledecomp/compare/lines.py
+++ reccmp/isledecomp/compare/lines.py
@@ -1,7 +1,6 @@
 """Database used to match (filename, line_number) pairs
 between FUNCTION markers and PDB analysis."""
 
-import sqlite3
 import logging
 from functools import cache
 from pathlib import Path, PurePath, PureWindowsPath
@@ -12,36 +11,12 @@ from reccmp.isledecomp.dir import convert_foreign_path
 
 logger = logging.getLogger(__name__)
 
-_SETUP_SQL = """
-    CREATE table lines (
-        addr integer not null primary key,
-        file integer not null,
-        lineno integer not null,
-        start integer not null default 0
-    ) without rowid;
-"""
-
-
-@cache
-def get_file_hash(filepath: str | Path | PurePath) -> int:
-    # This check is required for cross-platform tests.
-    # Don't convert a PurePosixPath back to PureWindowsPath if we are testing on a Windows host.
-    if isinstance(filepath, PurePath):
-        return hash(filepath)
-
-    return hash(PurePath(filepath))
-
 
 class LinesDb:
     def __init__(
         self, files: Sequence[Path] | Sequence[PurePath] | Sequence[str]
     ) -> None:
         self._path_resolver = cache(convert_foreign_path)
-
-        self._db = sqlite3.connect(":memory:")
-        self._db.executescript(_SETUP_SQL)
-
-        self._indexed = False
 
         # Set up memoized map of filenames to their paths
         self._filenames: dict[str, list[PurePath]] = {}
@@ -50,6 +25,14 @@ class LinesDb:
                 path = PurePath(path)
 
             self._filenames.setdefault(path.name.lower(), []).append(path)
+
+        # Local filename to list of (line_no, address) pairs
+        # This has to be a list instead of a dict because line numbers may be used twice.
+        # e.g. for the start and end of a loop.
+        self._map: dict[PurePath, list[tuple[int, int]]] = {}
+
+        # Addresses for the first line for a function
+        self._function_starts: set[int] = set()
 
     def add_line(self, foreign_path: PureWindowsPath, line_no: int, addr: int):
         """Connect the remote path to a line number and address pair."""
@@ -70,17 +53,10 @@ class LinesDb:
         if sourcepath is None:
             return
 
-        path_hash = hash(sourcepath)
-
-        self._db.executemany(
-            "INSERT into lines (addr, file, lineno) values (?,?,?)",
-            ((addr, path_hash, line_no) for (line_no, addr) in lines),
-        )
+        self._map.setdefault(sourcepath, []).extend(list(lines))
 
     def mark_function_starts(self, addrs: Sequence[int]):
-        self._db.executemany(
-            "UPDATE lines SET start = 1 WHERE addr = ?", ((addr,) for addr in addrs)
-        )
+        self._function_starts = self._function_starts.union(set(addrs))
 
     def search_line(
         self,
@@ -89,26 +65,18 @@ class LinesDb:
         line_end: int | None = None,
         start_only: bool = False,
     ) -> Iterator[int]:
-        # Add the index here, right before it's needed,
-        # so we don't incur overhead while inserting lines.
-        if not self._indexed:
-            self._db.execute("CREATE index idx_file_line on lines (file, lineno)")
-            self._indexed = True
-
         # If there is no end line, search for a single line only
         if line_end is None:
             line_end = line_start
 
-        path_hash = get_file_hash(local_path)
+        if not isinstance(local_path, PurePath):
+            local_path = PurePath(local_path)
 
-        for (addr,) in self._db.execute(
-            """
-            SELECT addr from lines
-            where file = ? and lineno >= ? and lineno <= ?
-            and (0 = ? or start = 1)""",
-            (path_hash, line_start, line_end, start_only),
-        ):
-            yield addr
+        for line_no, addr in self._map.get(local_path, []):
+            if (line_start <= line_no <= line_end) and (
+                not start_only or addr in self._function_starts
+            ):
+                yield addr
 
     def find_function(
         self,
