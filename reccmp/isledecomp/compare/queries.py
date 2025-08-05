5e5f8169638c5bd3dfd376965aff91ce66096aa0
MS <disinvite@users.noreply.github.com>
2025-08-04 22:12:49 -0400

    Match and name thunks using ref attribute (#181)
    * Match and name thunks using ref attribute
    
    * Fixes #176
    
    * Improve docstrings and naming

--- reccmp/isledecomp/compare/queries.py
+++ reccmp/isledecomp/compare/queries.py
@@ -24,11 +24,20 @@ def get_overloaded_functions(db: EntityDb) -> Iterable[OverloadedFunctionEntity]
         Row_number() OVER (partition BY json_extract(kvstore,'$.name') ORDER BY orig_addr nulls last, recomp_addr)
         from entities where json_extract(kvstore,'$.type') = ?
             and name in (
+            -- Subquery: build a list of names that are
+            -- repeated among FUNCTION entities.
             select json_extract(kvstore,'$.name') as name from entities
             where json_extract(kvstore,'$.type') = ?
             and name is not null
+            -- Ignore thunks (ref entities) because we only consider a name
+            -- to be non-unique if it is used by 2 or more real functions.
+            and ref_orig is null and ref_recomp is null
             group by name having count(name) > 1
-        )""",
+        )
+        -- Ignore any thunks using a name from our list of non-unique names.
+        -- We don't want to rename them at this stage.
+        and ref_orig is null and ref_recomp is null
+        """,
         (EntityType.FUNCTION, EntityType.FUNCTION),
     ):
         assert isinstance(orig_addr, int) or isinstance(recomp_addr, int)
@@ -36,3 +45,37 @@ def get_overloaded_functions(db: EntityDb) -> Iterable[OverloadedFunctionEntity]
         assert isinstance(symbol, str) or symbol is None
         assert isinstance(nth, int)
         yield OverloadedFunctionEntity(orig_addr, recomp_addr, name, symbol, nth)
+
+
+class ThunkWithName(NamedTuple):
+    """Entity address(es) and the name (computed or base name)
+    of the referenced (thunked) entity."""
+
+    orig_addr: int | None
+    recomp_addr: int | None
+    name: str
+
+
+def get_named_thunks(db: EntityDb):
+    """For each entity with a ref_orig or ref_recomp attribute,
+    if the parent entity is a function, return:
+        - one or both addresses of the referencing entity
+        - the name (or computed name) of the parent entity"""
+    for orig_addr, recomp_addr, name in db.sql.execute(
+        """SELECT e.orig_addr, e.recomp_addr,
+        coalesce(json_extract(r.kvstore, '$.computed_name'), json_extract(r.kvstore, '$.name')) name
+        FROM entities e
+        INNER JOIN entities r
+        ON e.ref_orig = r.orig_addr or e.ref_recomp = r.recomp_addr
+        WHERE name is not null
+        -- Do not return rows where (for example) orig_addr and ref_recomp are set.
+        -- If the entity has both ref_orig and ref_recomp, they must point to the same (matched) entity.
+        AND ((e.orig_addr is null and e.ref_orig is null) or e.ref_orig = r.orig_addr)
+        AND ((e.recomp_addr is null and e.ref_recomp is null) or e.ref_recomp = r.recomp_addr)
+        AND json_extract(r.kvstore, '$.type') = ?
+    """,
+        (EntityType.FUNCTION,),
+    ):
+        assert isinstance(orig_addr, int) or isinstance(recomp_addr, int)
+        assert isinstance(name, str)
+        yield ThunkWithName(orig_addr, recomp_addr, name)
