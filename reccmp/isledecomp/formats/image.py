86fe51cb19161009e90eaeeecb261c316f2fbc33
disinvite <disinvite@users.noreply.github.com>
2025-08-14 19:15:53 -0400

    Typos

--- reccmp/isledecomp/formats/image.py
+++ reccmp/isledecomp/formats/image.py
@@ -3,7 +3,7 @@ import dataclasses
 from pathlib import Path
 from .exceptions import InvalidVirtualReadError
 
-# Matches null-terminated string: 0-to-N null bytes and one null byte.
+# Matches 0-to-N non-null bytes and one byte null-terminator.
 r_szstring = re.compile(rb"[^\x00]*\x00")
 
 
@@ -17,7 +17,7 @@ class Image:
         """Must be implemented for each image.
         1. Go to the position in virtual memory for the given address.
         2. If it is valid, return a tuple with:
-            a. bytes or memoryview with tyhe relevant stream of data that begins at the address.
+            a. bytes or memoryview with the relevant stream of data that begins at the address.
             b. number of valid bytes remaining (including the size of whatever is in 'a').
         3. If the address is not valid, raise InvalidVirtualAddressError"""
         raise NotImplementedError
