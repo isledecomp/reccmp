2c2d5e7f631df89d657b86fba9af849082866c78
disinvite <disinvite@users.noreply.github.com>
2026-03-06 12:05:32 -0500

    Add tests for walk_source_dir

---
+++ tests/test_dir.py
@@ -0,0 +1,95 @@
+"""Tests for walking source code directories in search of relevant files"""
+
+from pathlib import Path
+import pytest
+from reccmp.dir import walk_source_dir
+
+
+def create_blank_file(p: Path):
+    """Helper to make this action more idiomatic in the tests."""
+    p.write_text("")
+
+
+@pytest.fixture(name="source_dir")
+def fixture_source_dir(tmp_path_factory) -> Path:
+    """Create a basic source root with files in two directories."""
+    src_dir = tmp_path_factory.mktemp("src")
+    (src_dir / "hello.cpp").write_text("")
+    (src_dir / "hello.hpp").write_text("")
+    (src_dir / "test").mkdir()
+    (src_dir / "test" / "game.cpp").write_text("")
+    (src_dir / "test" / "game.hpp").write_text("")
+
+    return src_dir
+
+
+def test_empty_dir(tmp_path_factory):
+    """Empty directory returns no files."""
+    path = tmp_path_factory.mktemp("empty")
+
+    files = list(walk_source_dir(path))
+    assert not files
+
+
+def test_dir_with_irrelevant_files(tmp_path_factory):
+    """Skip files that do not match our default filter on file extension."""
+    path = tmp_path_factory.mktemp("no_match")
+    create_blank_file(path / "test.txt")
+    create_blank_file(path / "sample.jpg")
+
+    files = list(walk_source_dir(path))
+    assert not files
+
+
+def test_dir_with_mixed_files(tmp_path_factory):
+    """Return code files and skip non-matching files."""
+    path = tmp_path_factory.mktemp("mixed")
+    create_blank_file(path / "test.txt")
+    create_blank_file(path / "sample.jpg")
+    create_blank_file(path / "game.cpp")
+    create_blank_file(path / "game.hpp")
+
+    files = list(walk_source_dir(path))
+    assert len(files) == 2
+    assert [f.name for f in files] == ["game.cpp", "game.hpp"]
+
+
+def test_recursion(tmp_path_factory):
+    """Should scan every subdirectory for matching files."""
+    path = tmp_path_factory.mktemp("recurse")
+    create_blank_file(path / "game.cpp")
+    (path / "x" / "y" / "z").mkdir(parents=True)
+    create_blank_file(path / "x" / "hello.cpp")
+    create_blank_file(path / "x" / "y" / "z" / "test.cpp")
+
+    files = list(walk_source_dir(path))
+    assert [f.name for f in files] == ["game.cpp", "hello.cpp", "test.cpp"]
+
+
+def test_recursion_disabled(tmp_path_factory):
+    """Should not enter subdirectories if recursion is disabled."""
+    path = tmp_path_factory.mktemp("recurse")
+    create_blank_file(path / "game.cpp")
+    (path / "x" / "y" / "z").mkdir(parents=True)
+    create_blank_file(path / "x" / "hello.cpp")
+    create_blank_file(path / "x" / "y" / "z" / "test.cpp")
+
+    files = list(walk_source_dir(path, recursive=False))
+    assert [f.name for f in files] == ["game.cpp"]
+
+
+def test_mixed_case_extension(tmp_path_factory):
+    """Should use case-insensitive match for file extensions."""
+    path = tmp_path_factory.mktemp("mixed_case")
+    create_blank_file(path / "game.CPP")
+    create_blank_file(path / "HELLO.C")
+    create_blank_file(path / "HELLO.h")
+    create_blank_file(path / "test.hpP")
+
+    files = list(walk_source_dir(path))
+    assert [f.name.lower() for f in files] == [
+        "game.cpp",
+        "hello.c",
+        "hello.h",
+        "test.hpp",
+    ]
