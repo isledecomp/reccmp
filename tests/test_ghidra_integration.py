0f69daf746c7a17ee35926f1201f6658ee97c240
jonschz <jonschz@users.noreply.github.com>
2026-03-14 19:50:51 +0100

    Increase ghidra test coverage

--- tests/test_ghidra_integration.py
+++ tests/test_ghidra_integration.py
@@ -10,13 +10,15 @@ from unittest.mock import Mock
 import pytest
 from reccmp.compare.core import Compare
 from reccmp.cvdump.cvinfo import CVInfoTypeEnum, CvdumpTypeKey, CvdumpTypeMap
-from reccmp.cvdump.types import CvdumpParsedType, FieldListItem
-from reccmp.ghidra.importer.exceptions import TypeNotFoundError
+from reccmp.cvdump.types import CvdumpParsedType, EnumItem, FieldListItem
+from reccmp.ghidra.importer.exceptions import TypeNotFoundError, TypeNotImplementedError
 from reccmp.ghidra.importer.pdb_extraction import PdbFunctionExtractor
 from tests.test_image_raw import RawImage
 
 if TYPE_CHECKING:
     from ghidra.program.flatapi import FlatProgramAPI
+    from ghidra.program.model.data import Structure
+    from reccmp.ghidra.importer.type_importer import PdbTypeImporter
 
 verified_types = (
     t
@@ -25,12 +27,21 @@ verified_types = (
 )
 
 
+@pytest.fixture(name="type_importer", scope="function")
+def pdb_type_importer_fixture(ghidra: "FlatProgramAPI"):
+    from reccmp.ghidra.importer.type_importer import PdbTypeImporter
+
+    compare = Compare(RawImage.from_memory(), RawImage.from_memory(), Mock(), "TEST")
+    type_importer = PdbTypeImporter(ghidra, PdbFunctionExtractor(compare), set())
+    yield type_importer
+
+
 @pytest.mark.parametrize("scalar_type", verified_types)
-def test_ghidra_scalar_types(ghidra, scalar_type):
+def test_ghidra_scalar_types(
+    type_importer: "PdbTypeImporter", scalar_type: CVInfoTypeEnum
+):
     from reccmp.ghidra.importer.type_importer import PdbTypeImporter
     from ghidra.program.model.data import Pointer
-
-    type_importer = PdbTypeImporter(ghidra, Mock(), set())
 
     cv_type_info = CvdumpTypeMap[scalar_type]
 
@@ -41,24 +52,27 @@ def test_ghidra_scalar_types(ghidra, scalar_type):
         assert isinstance(ghidra_type, Pointer)
 
 
-def test_ghidra_type_not_found(ghidra: "FlatProgramAPI"):
-    from reccmp.ghidra.importer.type_importer import PdbTypeImporter
-
-    compare = Compare(RawImage.from_memory(), RawImage.from_memory(), Mock(), "TEST")
-    type_importer = PdbTypeImporter(ghidra, PdbFunctionExtractor(compare), set())
-
+def test_ghidra_type_not_found(type_importer: "PdbTypeImporter"):
     with pytest.raises(TypeNotFoundError, match="Failed to find referenced type"):
         type_importer.import_pdb_type_into_ghidra(CvdumpTypeKey.from_str("0x1001"))
 
 
-def test_ghidra_type_class(ghidra: "FlatProgramAPI"):
-    from reccmp.ghidra.importer.type_importer import PdbTypeImporter
-    from ghidra.program.model.data import Structure
+forward_ref_key = CvdumpTypeKey.from_str("0x1001")
+class_field_list_key = CvdumpTypeKey.from_str("0x1002")
+class_key = CvdumpTypeKey.from_str("0x1003")
+pointer_to_class_key = CvdumpTypeKey.from_str("0x1004")
 
-    field_list_key = CvdumpTypeKey.from_str("0x1001")
-    class_key = CvdumpTypeKey.from_str("0x1002")
-    compare = Compare(RawImage.from_memory(), RawImage.from_memory(), Mock(), "TEST")
-    compare.types.keys[field_list_key] = CvdumpParsedType(
+
+def _set_up_test_class(compare: Compare):
+    compare.types.keys[forward_ref_key] = CvdumpParsedType(
+        type="LF_CLASS",
+        name="TestClass",
+        field_list_type=CVInfoTypeEnum.T_NOTYPE,
+        size=0,
+        udt=class_key,
+        is_forward_ref=True,
+    )
+    compare.types.keys[class_field_list_key] = CvdumpParsedType(
         type="LF_FIELDLIST",
         members=[
             FieldListItem(offset=0, name="id", type=CVInfoTypeEnum.T_INT4),
@@ -66,22 +80,203 @@ def test_ghidra_type_class(ghidra: "FlatProgramAPI"):
         ],
     )
     compare.types.keys[class_key] = CvdumpParsedType(
-        type="LF_CLASS", name="TestClass", field_list_type=field_list_key, size=8
+        type="LF_CLASS", name="TestClass", field_list_type=class_field_list_key, size=8
     )
-    type_importer = PdbTypeImporter(ghidra, PdbFunctionExtractor(compare), set())
+    compare.types.keys[pointer_to_class_key] = CvdumpParsedType(
+        type="LF_POINTER", element_type=class_key
+    )
 
-    x = type_importer.import_pdb_type_into_ghidra(class_key)
 
-    assert isinstance(x, Structure)
-    assert x.length == 8
+def _assert_test_class(imported_structure: "Structure"):
+    from ghidra.program.model.data import Structure
 
-    [id_component, name_component] = list(x.getComponents())
+    assert isinstance(imported_structure, Structure)
+
+    assert imported_structure.getDisplayName() == "TestClass"
+    assert imported_structure.length == 8
+
+    [id_component, name_component] = list(imported_structure.getComponents())
     assert id_component.getOffset() == 0
     assert id_component.getDataType().name == "int"
     assert name_component.getOffset() == 4
     assert name_component.getDataType().name == "char *"
 
 
+def test_ghidra_type_class(type_importer: "PdbTypeImporter"):
+    _set_up_test_class(type_importer.extraction.compare)
+    imported_structure = type_importer.import_pdb_type_into_ghidra(class_key)
+    _assert_test_class(imported_structure)
+
+
 def test_ghidra_verify_test_isolation(ghidra: "FlatProgramAPI"):
     """Make sure that the `TestClass` created above was rolled back."""
     assert not list(ghidra.getDataTypes("TestClass"))
+
+
+def test_ghidra_forward_ref_to_pdb_type(type_importer: "PdbTypeImporter"):
+    _set_up_test_class(type_importer.extraction.compare)
+    imported_structure = type_importer.import_pdb_type_into_ghidra(forward_ref_key)
+    _assert_test_class(imported_structure)
+
+
+def test_forward_ref_to_missing_type(type_importer: "PdbTypeImporter"):
+    type_importer.extraction.compare.types.keys[forward_ref_key] = CvdumpParsedType(
+        type="LF_STRUCTURE",
+        name="HWND__",
+        field_list_type=CVInfoTypeEnum.T_NOTYPE,
+        size=0,
+        is_forward_ref=True,
+    )
+
+    with pytest.raises(
+        TypeNotImplementedError,
+        match="forward ref without target, needs to be created manually:",
+    ):
+        type_importer.import_pdb_type_into_ghidra(forward_ref_key)
+
+
+def test_forward_ref_to_pre_existing_type(
+    ghidra: "FlatProgramAPI", type_importer: "PdbTypeImporter"
+):
+    data_type_manager = ghidra.getCurrentProgram().getDataTypeManager()
+    from ghidra.program.model.data import (
+        TypedefDataType,
+        VoidDataType,
+        DataTypeConflictHandler,
+    )
+
+    hwnd = data_type_manager.addDataType(
+        TypedefDataType("HWND__", VoidDataType()), DataTypeConflictHandler.KEEP_HANDLER
+    )
+
+    type_importer.extraction.compare.types.keys[forward_ref_key] = CvdumpParsedType(
+        type="LF_STRUCTURE",
+        name="HWND__",
+        field_list_type=CVInfoTypeEnum.T_NOTYPE,
+        size=0,
+        is_forward_ref=True,
+    )
+
+    imported_hwnd = type_importer.import_pdb_type_into_ghidra(forward_ref_key)
+    assert imported_hwnd == hwnd
+
+
+def test_ghidra_pointer_to_class(type_importer: "PdbTypeImporter"):
+    from ghidra.program.model.data import Pointer
+
+    _set_up_test_class(type_importer.extraction.compare)
+    imported_pointer: Pointer = type_importer.import_pdb_type_into_ghidra(
+        pointer_to_class_key
+    )
+
+    assert isinstance(imported_pointer, Pointer)
+    _assert_test_class(imported_pointer.dataType)
+
+
+def test_array(type_importer: "PdbTypeImporter"):
+    from ghidra.program.model.data import Array
+
+    array_key = CvdumpTypeKey.from_str("0x1005")
+    type_importer.extraction.compare.types.keys[array_key] = CvdumpParsedType(
+        type="LF_ARRAY", array_type=CVInfoTypeEnum.T_INT4, size=16
+    )
+
+    imported_array: Array = type_importer.import_pdb_type_into_ghidra(array_key)
+
+    assert isinstance(imported_array, Array)
+    assert imported_array.getLength() == 16
+    assert imported_array.getElementLength() == 4
+    assert imported_array.getDataType() == type_importer.import_pdb_type_into_ghidra(
+        CVInfoTypeEnum.T_INT4
+    )
+
+
+def test_enum(type_importer: "PdbTypeImporter"):
+    from ghidra.program.model.data import Enum
+
+    enum_fieldlist_key = CvdumpTypeKey.from_str("0x1006")
+    enum_key = CvdumpTypeKey.from_str("0x1007")
+
+    type_importer.extraction.compare.types.keys[enum_fieldlist_key] = CvdumpParsedType(
+        type="LF_FIELDLIST",
+        variants=[
+            EnumItem(name="c_zero", value=0),
+            EnumItem(name="c_one", value=1),
+        ],
+    )
+    type_importer.extraction.compare.types.keys[enum_key] = CvdumpParsedType(
+        name="TestEnum",
+        type="LF_ENUM",
+        field_list_type=enum_fieldlist_key,
+        is_nested=False,
+        num_members=2,
+        underlying_type=CVInfoTypeEnum.T_INT4,
+    )
+
+    imported_enum: Enum = type_importer.import_pdb_type_into_ghidra(enum_key)
+    assert isinstance(imported_enum, Enum)
+    assert imported_enum.getDisplayName() == "TestEnum"
+    assert imported_enum.getCount() == 2
+    assert list(imported_enum.getNames()) == ["c_zero", "c_one"]
+    assert list(imported_enum.getValues()) == [0, 1]
+
+
+def test_fallback_procedure_import(type_importer: "PdbTypeImporter"):
+    """The feature is not fully implemented. This test asserts on the fallback behaviour."""
+
+    arglist_key = CvdumpTypeKey.from_str("0x1008")
+    procedure_key = CvdumpTypeKey.from_str("0x1009")
+    _set_up_test_class(type_importer.extraction.compare)
+
+    type_importer.extraction.compare.types.keys[arglist_key] = CvdumpParsedType(
+        type="LF_ARGLIST", argcount=1, args=[pointer_to_class_key]
+    )
+    type_importer.extraction.compare.types.keys[procedure_key] = CvdumpParsedType(
+        type="LF_PROCEDURE",
+        return_type=CVInfoTypeEnum.T_VOID,
+        call_type="C Near",
+        func_attr="none",
+        num_params=1,
+        arg_list_type=arglist_key,
+    )
+    imported_type = type_importer.import_pdb_type_into_ghidra(procedure_key)
+    # Fallback behaviour. This assertion should be changed if proper support is implemented
+    assert imported_type == type_importer.import_pdb_type_into_ghidra(
+        CVInfoTypeEnum.T_VOID
+    )
+
+
+@pytest.mark.xfail(reason="Union import not yet implemented")
+def test_union(type_importer: "PdbTypeImporter"):
+    from ghidra.program.model.data import Union
+
+    union_field_list_key = CvdumpTypeKey.from_str("0x100a")
+    union_key = CvdumpTypeKey.from_str("0x100b")
+
+    type_importer.extraction.compare.types.keys[union_field_list_key] = (
+        CvdumpParsedType(
+            type="LF_FIELDLIST",
+            members=[
+                FieldListItem(offset=0, name="byte", type=CVInfoTypeEnum.T_32PUCHAR),
+                FieldListItem(offset=0, name="word", type=CVInfoTypeEnum.T_32PUSHORT),
+            ],
+        )
+    )
+    type_importer.extraction.compare.types.keys[union_key] = CvdumpParsedType(
+        type="LF_UNION",
+        num_members=2,
+        field_list_type=union_field_list_key,
+        is_nested=True,
+        size=4,
+        name="MY_ENUM",
+    )
+
+    imported_union: Union = type_importer.import_pdb_type_into_ghidra(union_key)
+    assert isinstance(imported_union, Union)
+
+    assert imported_union.getDisplayName() == "MY_ENUM"
+    assert imported_union.getLength() == 4
+    assert list(imported_union.getComponents()) == [
+        type_importer.import_pdb_type_into_ghidra(CVInfoTypeEnum.T_32PUCHAR),
+        type_importer.import_pdb_type_into_ghidra(CVInfoTypeEnum.T_32PUSHORT),
+    ]
