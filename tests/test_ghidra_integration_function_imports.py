0cf2f0bfa955762377c32a4398ffb9e4b994e45f
jonschz <jonschz@users.noreply.github.com>
2026-04-18 13:04:47 +0200

    Refactor to cvdump-based approach, part 1

--- tests/test_ghidra_integration_function_imports.py
+++ tests/test_ghidra_integration_function_imports.py
@@ -8,10 +8,12 @@ import json
 from typing import TYPE_CHECKING, Iterator
 
 import pytest
+from reccmp.compare.ingest import load_cvdump_types
+from reccmp.cvdump.analysis import CvdumpAnalysis
 from reccmp.cvdump.cvinfo import CVInfoTypeEnum, CvdumpTypeKey
 from reccmp.compare.db import ReccmpMatch
 
-from reccmp.cvdump.types import CvdumpParsedType, FieldListItem
+from reccmp.cvdump.parser import CvdumpParser
 from reccmp.ghidra.importer.pdb_extraction import (
     CppRegisterSymbol,
     CppStackSymbol,
@@ -25,13 +27,6 @@ if TYPE_CHECKING:
 
 ORIG_FN_TO_OVERWRITE_PRIMARY = 0x00402880  # readIntFromRegistry()
 ORIG_FN_TO_OVERWRITE_SECONDARY = 0x00402C20  # IsleApp::Tick()
-
-
-class_field_list_key = CvdumpTypeKey.from_str("0x1002")
-class_key = CvdumpTypeKey.from_str("0x1003")
-anim_actor_entry_key = CvdumpTypeKey.from_str("0x1004")
-anim_actor_entry_pointer_key = CvdumpTypeKey.from_str("0x1005")
-anim_actor_entry_field_list_key = CvdumpTypeKey.from_str("0x1006")
 
 
 class GhidraFunctionTestHelper:
@@ -139,41 +134,50 @@ def test_record_array_access(
         PdbFunction,
     )
 
+    # Shortened version of a BETA10 recompilation
+    cvdump_types = """
+0x12c8 : Length = 42, Leaf = 0x1505 LF_STRUCTURE
+	# members = 0,  field list type 0x0000, FORWARD REF,
+	Derivation list type 0x0000, VT shape type 0x0000
+	Size = 0, class name = LegoAnimActorEntry, UDT(0x00006081)
+
+0x12c9 : Length = 10, Leaf = 0x1002 LF_POINTER
+	Pointer (NEAR32), Size: 0
+	Element type : 0x12C8
+
+0x12cd : Length = 314, Leaf = 0x1203 LF_FIELDLIST
+    list[10] = LF_MEMBER, protected, type = T_LONG(0012), offset = 8
+		member name = 'm_duration'
+	list[11] = LF_MEMBER, protected, type = 0x12C9, offset = 12
+		member name = 'm_modelList'
+	list[12] = LF_MEMBER, protected, type = T_ULONG(0022), offset = 16
+		member name = 'm_numActors'
+
+0x12cf : Length = 30, Leaf = 0x1504 LF_CLASS
+	# members = 15,  field list type 0x12cd, CONSTRUCTOR,
+	Derivation list type 0x0000, VT shape type 0x12ce
+	Size = 24, class name = LegoAnim, UDT(0x000012cf)
+
+0x6080 : Length = 62, Leaf = 0x1203 LF_FIELDLIST
+	list[1] = LF_MEMBER, public, type = T_32PRCHAR(0470), offset = 0
+		member name = 'm_name'
+	list[2] = LF_MEMBER, public, type = T_ULONG(0022), offset = 4
+		member name = 'm_type'
+
+0x6081 : Length = 42, Leaf = 0x1505 LF_STRUCTURE
+	# members = 3,  field list type 0x6080,
+	Derivation list type 0x0000, VT shape type 0x0000
+	Size = 8, class name = LegoAnimActorEntry, UDT(0x00006081)
+    """
+
+    legoanim_class_key = CvdumpTypeKey(0x12CF)
+
     compare = type_importer.extraction.compare
-    # LegoAnimActorEntry
-    compare.types.keys[anim_actor_entry_field_list_key] = CvdumpParsedType(
-        type="LF_FIELDLIST",
-        members=[
-            FieldListItem(offset=0, name="m_name", type=CVInfoTypeEnum.T_32PCHAR),
-            FieldListItem(offset=4, name="m_type", type=CVInfoTypeEnum.T_INT4),
-        ],
-    )
-    compare.types.keys[anim_actor_entry_key] = CvdumpParsedType(
-        type="LF_CLASS",
-        name="LegoAnimActorEntry",
-        field_list_type=anim_actor_entry_field_list_key,
-        size=8,
-    )
-    compare.types.keys[anim_actor_entry_pointer_key] = CvdumpParsedType(
-        type="LF_POINTER", element_type=anim_actor_entry_key
-    )
-    # LegoAnim (shortened)
-    compare.types.keys[class_field_list_key] = CvdumpParsedType(
-        type="LF_FIELDLIST",
-        members=[
-            FieldListItem(offset=0, name="id", type=CVInfoTypeEnum.T_INT4),
-            FieldListItem(offset=4, name="name", type=CVInfoTypeEnum.T_32PCHAR),
-            FieldListItem(
-                offset=0xC, name="m_modelList", type=anim_actor_entry_pointer_key
-            ),
-        ],
-    )
-    compare.types.keys[class_key] = CvdumpParsedType(
-        type="LF_CLASS",
-        name="LegoAnim",
-        field_list_type=class_field_list_key,
-        size=0x10,
-    )
+
+    parser = CvdumpParser()
+    parser.read_section("TYPES", cvdump_types)
+    analysis = CvdumpAnalysis(parser)
+    load_cvdump_types(analysis, compare.types)
 
     # shortened version of LEGO1 0x100a0f20
     function_helper.overwrite_example_function(
@@ -187,9 +191,9 @@ def test_record_array_access(
         call_type="__thiscall",
         arglist=[CVInfoTypeEnum.T_INT4],
         return_type=CVInfoTypeEnum.T_32PCHAR,
-        class_type=class_key,
+        class_type=legoanim_class_key,
         stack_symbols=[
-            CppRegisterSymbol("this", class_key, "ecx"),
+            CppRegisterSymbol("this", legoanim_class_key, "ecx"),
             CppStackSymbol("p_index", CVInfoTypeEnum.T_INT4, 4),
         ],
         this_adjust=0,
