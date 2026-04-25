0e18c16f85a0ff7db0906883860ceea423460580
Christian Semmler <mail@csemmler.com>
2026-04-25 09:42:55 -0700

    Use lower()

--- reccmp/utils.py
+++ reccmp/utils.py
@@ -313,7 +313,7 @@ def diff_json(
     """Compare two status report files, determine what items changed, and print the result."""
 
     # Don't try to diff a report generated for a different binary file
-    if saved_data.filename.casefold() != new_data.filename.casefold():
+    if saved_data.filename.lower() != new_data.filename.lower():
         logging.getLogger().error(
             "Diff report for '%s' does not match current file '%s'",
             saved_data.filename,
